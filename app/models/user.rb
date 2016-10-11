14b3a175b5f9f696bd52207f7fc3dcf790f4a18b
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-10 20:58:40 -0400

    Added user authentication and tests

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :email, presence: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+    uniqueness: { case_sensitive: false }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+   role == "admin"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should have_valid(:first_name).when('John', 'Sally') }
+  it { should_not have_valid(:first_name).when(nil, '') }
+
+  it { should have_valid(:last_name).when('Smith', 'Williams') }
+  it { should_not have_valid(:last_name).when(nil, '') }
+
+  it { should have_valid(:username).when('username1', 'testuser') }
+  it { should_not have_valid(:username).when(nil, '') }
+
+  it { should have_valid(:email).when('user@example.com', 'test@test.com') }
+  it { should_not have_valid(:email).when(nil, '', 'user', 'user@', '.com', 'user@example', 'user.com') }
+
+  it 'has a matching password confirmation for the password' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
+end
+
+describe "#admin?" do
+  it "is not an admin if the role is not admin" do
+    user = FactoryGirl.create(:user, role: "member")
+    expect(user.admin?).to eq(false)
+  end
+
+  it "is an admin if the role is admin" do
+    user = FactoryGirl.create(:user, role: "admin")
+    expect(user.admin?).to eq(true)
+  end
+end
