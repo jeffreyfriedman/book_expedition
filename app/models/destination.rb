426018598cf8d692797b61dc8b0beaadc341179b
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-19 12:36:20 -0400

    added check for timed-out photo retrieval

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -8,14 +8,19 @@ class Destination < ActiveRecord::Base
     api_key = ENV['FLICKR_KEY']
     formatted_keywords = "#{keywords[:country]}" + "#{keywords[:city].to_s}"
     photos = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{api_key}&text=#{formatted_keywords}%20landmark&sort=relevance&license=1%2C2%2C3%2C4%2C5%2C6%2C7%2C8&safe_search=1&content_type=1&media=photos&format=json&nojsoncallback=1")
-    first_photo = photos["photos"]["photo"][0]
+    if !photos.nil?
+      first_photo = photos["photos"]["photo"][0]
 
-    owner_info = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=#{api_key}&photo_id=#{first_photo['id']}&format=json&nojsoncallback=1")
+      owner_info = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=#{api_key}&photo_id=#{first_photo['id']}&format=json&nojsoncallback=1")
 
-    photo_url = "https://farm#{first_photo['farm']}.staticflickr.com/#{first_photo['server']}/#{first_photo['id']}_#{first_photo['secret']}.jpg"
+      photo_url = "https://farm#{first_photo['farm']}.staticflickr.com/#{first_photo['server']}/#{first_photo['id']}_#{first_photo['secret']}.jpg"
 
-    owner = owner_info["photo"]["urls"]["url"][0]["_content"]
-
+      owner = owner_info["photo"]["urls"]["url"][0]["_content"]
+    else
+      photo_url = ""
+      owner = ""
+    end
+    
     blurb = ""
     return { blurb: blurb, image: photo_url, owner: owner }
   end
--- spec/features/05_api_create_destinations_spec.rb
+++ spec/features/05_api_create_destinations_spec.rb
@@ -3,15 +3,16 @@ feature "create destinations through API" do
 
     let!(:user) { FactoryGirl.create(:user) }
     describe 'POST /api/v1/destinations' do
-      country = 'Japan'
+      country = 'japan'
       it 'creates a new Destination and UserDestination if a valid destination is provided' do
         sign_in user
         post :create, params: {destination: {country: country}}
 
         expect(response.status).to eq(201)
         res_body = JSON.parse(response.body)
-        expect(res_body['destination']['country']).to eq(country)
+        expect(res_body['destination']['country']).to eq(country.capitalize)
         expect(res_body['destination']['id']).not_to be(nil)
+        expect(res_body['destination']['image']).not_to be(nil)
 
         user_destination = UserDestination.first
         expect(user_destination.user_id).to eq(user.id)
