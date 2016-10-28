f38b5e02a2e6d7b541856787ec5794d42e7a2048
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-27 20:37:46 -0400

    added D3 radial dendrogram data visualization to dashboard

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Api::V1::UsersController < ApiController
   def index
     unless current_user.nil?
@@ -6,13 +8,32 @@ class Api::V1::UsersController < ApiController
       user_books = current_user.books.order(created_at: :desc)
       user_destination_notes = current_user.user_destinations.order(created_at: :desc)
       user_booknotes = current_user.user_books.order(created_at: :desc)
+      user_datamap = generate_datamap
     end
+
     render json: {
       user_info: user_info,
       destinations: user_destinations,
       destination_notes: user_destination_notes,
       books: user_books,
-      book_notes: user_booknotes
+      book_notes: user_booknotes,
+      datamap: user_datamap
     }, status: :ok
+  end
+
+  private
+  def generate_datamap
+    username = current_user.username
+    datamap_array = [JSON.parse('{"id": "'"#{username}"'", "value":""}')]
+    current_user.destinations.each do |destination|
+      destination_value = "#{username}.#{destination.country}"
+      datamap_array << JSON.parse('{"id": "'"#{destination_value}"'", "value":""}')
+    end
+    current_user.books.each do |book|
+      book_value = "#{username}.#{book.destinations[0].country}.#{book.title}"
+      datamap_array << JSON.parse('{"id": "'"#{book_value}"'", "value":""}')
+    end
+
+    return datamap_array
   end
 end
