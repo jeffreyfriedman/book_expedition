48e28bfcf0f0b8499d611d40c2f103b4f0ec6b58
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-30 17:52:53 -0400

    added description to splash page, fixed bug where entered city was not shown in destination card

--- react/src/components/DestinationTitle.js
+++ react/src/components/DestinationTitle.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router';
 import DeleteDestinationButton from './DeleteDestinationButton';
 
 const DestinationTitle = props => {
-  let detailPath = `/destinations/${props.id}`
+  let detailPath = `/destinations/${props.id}`;
+  let destinationTitle;
+  if (props.city.length > 0) {
+    destinationTitle = `${props.city}, ${props.country}`
+  } else {
+    destinationTitle = `${props.country}`
+  }
   return(
     <div className="col s6 m4">
       <div className="card medium hoverable">
         <Link to={detailPath}>
           <div className="card-image">
             <img src={props.image} />
-            <span className="card-title indigo lighten-4">{props.city} {props.country}</span>
+            <span className="card-title indigo lighten-4">{destinationTitle}</span>
           </div>
         </Link>
         <div className="card-content">
